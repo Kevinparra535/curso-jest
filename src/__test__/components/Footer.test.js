2e6277be2676c2ea178425cb5230db01772ddc02
Kevin Parra <kevinparra535@gmail.com>
2021-09-23 08:19:00 -0500

    [ADD] SNAPSHOTS

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,5 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { mount } from 'enzyme';
+import { create } from 'react-test-renderer';
 import Footer from '../../components/Footer';
 
 describe('Footer', () => {
@@ -11,5 +13,13 @@ describe('Footer', () => {
 
   test('Render del titulo', () => {
     expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Footer Snapshot', () => {
+  test('Comprobar la UI del componentn Footer', () => {
+    const footer = create(<Footer />);
+
+    expect(footer.toJSON()).toMatchSnapshot();
   });
 });
--- src/__test__/components/Header.test.js
+++ src/__test__/components/Header.test.js
@@ -1,5 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { mount, shallow } from 'enzyme';
+import { create } from 'react-test-renderer';
 import ProviderMock from '../../__mocks__/ProviderMock';
 import Header from '../../components/Header';
 
@@ -22,5 +24,17 @@ describe('<Header />', () => {
     );
 
     expect(header.find('.Header-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Header SnapShot', () => {
+  test('Comprobar el Snapshot de Header', () => {
+    const header = create(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>,
+    );
+
+    expect(header.toJSON()).toMatchSnapshot();
   });
 });
